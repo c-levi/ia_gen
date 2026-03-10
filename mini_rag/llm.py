eadd2347ce0513d55c8f93a1c80af5ea7b588dd9
clevi@5degres.com <clevi@5degres.com>
2026-03-10 08:56:35 +0100

    feat: use smaller llm

--- mini_rag/llm.py
+++ mini_rag/llm.py
@@ -1,11 +1,21 @@
 import requests
 
 def ask_ollama(prompt):
+
+    model = "phi3:mini" # "mistral", #"llama3.2:3b", 
+
+    print(f"Model used: {model}")
+    print(f"Prompt length (chars): {len(prompt)}")
+
     url = "http://localhost:11434/api/generate"
     data = {
-        "model": "mistral", #"llama3.2:3b", 
+        "model": model,
         "prompt": prompt,
-        "stream": False
+        "stream": False,
+        "options": {
+            # "num_predict": 100,   # limite la taille de réponse
+            "temperature": 0.2
+        }
     }
     response = requests.post(url, json=data)
     return response.json()["response"]
